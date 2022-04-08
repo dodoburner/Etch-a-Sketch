ab1bb9a5421a27ee53132ae531eb53a6877bbf4e
Dorian <uremdorian@gmail.com>
2022-04-08 13:03:50 +0200

    finished project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Etch-a-Sketch
+This project will be something like a sketch pad. I am doing this project to demonstrate what I learned about DOM manipulation and to learn something new during my project.
---
+++ index.js
@@ -0,0 +1,84 @@
+let grid= document.createElement('div');
+grid.setAttribute('id', 'grid');
+grid.style.display= 'grid';
+grid.style.borderWidth= '3px';
+grid.style.borderColor='black';
+grid.style.borderStyle='solid';
+grid.style.width= '800px'
+grid.style.height='800px'
+grid.style.gridTemplateColumns= 'repeat(16, 1fr)';
+grid.style.gridTemplateRows= 'repeat(16, 1fr)';
+
+let divArray=[];
+let div;
+
+for (let i=0; i<256; i++) {
+    div= document.createElement('div');
+    div.setAttribute('class', 'divs')
+    div.style.borderWidth= '1px';
+    div.style.borderColor='black';
+    div.style.borderStyle='solid';
+    divArray.push(div);
+    grid.appendChild(div)
+}
+
+let body= document.querySelector('body');
+body.appendChild(grid);
+
+
+
+divArray.forEach(i => i.addEventListener('mouseenter',
+ function (e) {
+    i.style.backgroundColor= 'blue';
+}
+));
+
+let button= document.createElement('button');
+body.appendChild(button);
+button.textContent= 'NEW GRID';
+
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+button.addEventListener('click', function(e) {
+    removeAllChildNodes(grid);
+    divArray= [];
+    let num= prompt('How many squares?(max 100)', '');
+    if (num > 100 || num < 0 || isNaN(num)) {
+       return alert('Please enter a valid input')
+    }
+
+    let squares= num * num;
+    grid.style.gridTemplateColumns= 'repeat(' + num + ',1fr)'
+    grid.style.gridTemplateRows= 'repeat(' + num + ',1fr)'
+   
+    for (let i=0; i<squares; i++) {
+        div= document.createElement('div');
+        div.setAttribute('class', 'divs')
+        div.style.borderWidth= '1px';
+        div.style.borderColor='black';
+        div.style.borderStyle='solid';
+        divArray.push(div);
+        grid.appendChild(div)
+    }
+
+    divArray.forEach(i => i.addEventListener('mouseenter',
+ function (e) {
+    i.style.backgroundColor= 'blue';
+}
+));
+})
+
+
+
+
+
+
+
+
+
+
